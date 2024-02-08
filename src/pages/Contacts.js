617898145072d0e200c28183f38d2c521f526a1f
Daniel Adylov <adylovdaniel7@gmail.com>
2024-02-08 19:41:32 +0600

    'initial'

---
+++ src/pages/Contacts.js
@@ -0,0 +1,32 @@
+const Contacts = () => {
+    return (
+		<main className="section">
+			<div className="container">
+				<h1 className="title-1">Contacts</h1>
+
+				<ul className="content-list">
+					<li className="content-list__item">
+						<h2 className="title-2">Location</h2>
+						<p>Kyrgyzstan, Bishkek</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Telegram / WhatsApp</h2>
+						<p>
+							<a href="tel:+99650700732">+996 507 007 321</a>
+						</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Email</h2>
+						<p>
+							<a href="mailto:webdev@protonmail.com">
+								adylovdaniel7@gmail.com
+							</a>
+						</p>
+					</li>
+				</ul>
+			</div>
+		</main>
+	);
+}
+
+export default Contacts;
